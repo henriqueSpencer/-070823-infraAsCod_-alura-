13e1f1f0db651b5524b0b827cf627d8e66375fbd
henriqueSpencer <henriquespencer11@gmail.com>
2023-08-09 08:36:46 -0300

    ansible funcionando

--- main.tf
+++ main.tf
@@ -21,7 +21,15 @@ resource "aws_instance" "app_server" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   key_name      = "estudoalura"
+  # Codigo shell executado dps da criacao
+  # user_data = <<-EOF
+  #               #!/bin/bash
+  #               cd /home/ubuntu
+  #               echo “<h1>Mensagem a ser mostrada</h1>” > index.html
+  #               nohup busybox httpd -f -p 8080 &
+  #               EOF
   tags = {
-    Name = "Primeira Instância"
+    Name = "Teste aws"
   }
 }
+
